cf057546f1b8758d251270ecb81a1e64cc81158c
Sean Francis N. Ballais <sean@seanballais.com>
2016-08-02 18:44:00 +0800

    Added Season 1 download support. Adding other seasons in the future.

---
+++ app.py
@@ -0,0 +1,27 @@
+import urllib2
+
+def main():
+    # List of episode titles.
+    URL = 'http://traffic.libsyn.com/wolf359radio/'
+
+    season_1_links = [
+        'Succulent_Rat-Killing_Tar',
+        '2_-_Little_Revolucion',
+        '3_-_Discomforts_Pains_and_Irregularities',
+        'Cataracts_and_Hurricanoes',
+        'Cigarette_Candy',
+        'Super_Energy_Saver_Mode',
+        'The_Sound_and_the_Fury',
+        'AreSpaceSuitsItchy',
+        'Box_953',
+        'The_Empty_Man_Cometh',
+        'Extreme_Danger_Bug',
+        'Am_I_Alone_Now',
+        'Deep_Breaths',
+        'Gas_Me_Twice'
+    ]
+
+    pass
+
+if __name__ == '__main__':
+    main()
